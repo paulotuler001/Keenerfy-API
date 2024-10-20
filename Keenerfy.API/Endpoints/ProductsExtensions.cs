18c2b3269c5d82287f3a74c47ab3c8f0e3727435
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-20 15:31:22 -0300

    feat: extension from Program.cs with the complete crud of Products

---
+++ Keenerfy.API/Endpoints/ProductsExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Keenerfy.API.Requests;
+using Keenerfy.Keenerfy.Database;
+using Keenerfy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keenerfy.API.Endpoints;
+public static class ProductsExtensions
+{
+    public static void ProductsEndpoints(this WebApplication app)
+    {
+        app.MapGet("/products", ([FromServices] DAL<Product> dal) =>
+        {
+            return dal.List();
+        });
+
+        app.MapGet("/products/{name}", ([FromServices] DAL<Product> dal, string name) =>
+        {
+            return dal.FindBy(a => a.Name.ToUpper().Equals(name.ToUpper()));
+        });
+
+        app.MapPost("/products", ([FromServices] DAL<Product> dal, [FromBody] ProductsRequest productRequest) =>
+        {
+            var product = new Product(productRequest.Name, productRequest.Code, productRequest.Description, productRequest.Price, productRequest.Link, productRequest.Stock_id);
+            dal.Create(product);
+            return Results.Ok(product);
+        });
+
+        app.MapPut("/products", ([FromServices] DAL<Product> dal, [FromBody] ProductsRequestEdit productRequestEdit) =>
+        {
+            var productToUpdate = dal.FindBy(a => a.Id.Equals(productRequestEdit.Id));
+            if (productToUpdate is null)
+            {
+                return Results.NotFound();
+            }
+            dal.Update(productToUpdate);
+            return Results.Ok(productToUpdate);
+        });
+    }
+}
