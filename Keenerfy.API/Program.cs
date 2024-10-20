6029617f41a8a2fd7b7cdcafc0fc017d657dc7fa
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-20 12:33:48 -0300

    feat: Addin get, get-by-name and post and depency injection in products requests

--- Keenerfy.API/Program.cs
+++ Keenerfy.API/Program.cs
@@ -1,16 +1,32 @@
 using Keenerfy.Database;
 using Keenerfy.Keenerfy.Database;
 using Keenerfy.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlTypes;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<KeenerfyContext>();
+builder.Services.AddTransient<DAL<Product>>();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () => 
+app.MapGet("/products", ([FromServices] DAL<Product> dal) => 
 {
-    var dal = new DAL<Product>(new KeenerfyContext());
     return dal.Listar();
+});
+
+app.MapGet("/products/{name}", ([FromServices] DAL < Product > dal, string name) =>
+{
+    return dal.FindBy(a => a.Name.ToUpper().Equals(name.ToUpper()));
+});
+
+app.MapPost("/products", ([FromServices] DAL < Product > dal, [FromBody] Product product) =>
+{
+    dal.Adicionar(product);
+    return Results.Ok(product);
 });
 
 app.Run();
