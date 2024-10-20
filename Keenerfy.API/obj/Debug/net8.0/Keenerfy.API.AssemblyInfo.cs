0d5c5b4c8963bb00d34f049a6845baf73853c527
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-20 12:31:18 -0300

    docs: Updated files made by visual studio that i dont know what they do

--- Keenerfy.API/obj/Debug/net8.0/Keenerfy.API.AssemblyInfo.cs
+++ Keenerfy.API/obj/Debug/net8.0/Keenerfy.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Keenerfy.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba468daa793fbc6ee0a482273787ec578dfe191")]
 [assembly: System.Reflection.AssemblyProductAttribute("Keenerfy.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Keenerfy.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
