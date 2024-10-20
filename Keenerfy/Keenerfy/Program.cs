684cb2efacc2f9ab8bc6321a12e12fdb8f64b634
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-20 10:57:14 -0300

    fix: Adding the right Keenerfy initial folder

---
+++ Keenerfy/Keenerfy/Program.cs
@@ -0,0 +1,15 @@
+﻿using Keenerfy.Database;
+using Keenerfy.Keenerfy.Database;
+using Keenerfy.Keenerfy.Service;
+using Keenerfy.Models;
+
+var context = new KeenerfyContext();
+
+ProductService prod = new();
+
+DAL<Product> product = new(context);
+
+//prod.NewProduct(product);
+prod.GetAll(product);
+//prod.UpdateProduct(product);
+//prod.RemoveProduct(product);
---
+++ Keenerfy/Keenerfy/Service/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using Keenerfy.Keenerfy.Database;
+using Keenerfy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keenerfy.Keenerfy.Service;
+internal class ProductService
+{
+    public void NewProduct(DAL<Product> product)
+    {
+        Product product1 = new();
+
+        Console.WriteLine("Digite o nome do seu novo produto");
+        string product_name = Console.ReadLine();
+        Console.WriteLine("Descrição");
+        string description = Console.ReadLine();
+        Console.WriteLine("Code");
+        string code = Console.ReadLine();
+        product1.Name = product_name;
+        product1.Description = description;
+        product1.Code = code.ToString();
+        product1.Price = 105.4f;
+        product.Adicionar(product1);
+    }
+    public IEnumerable<Product> GetAll(DAL<Product> product)
+    {
+        return product.Listar();
+    }
+    public void UpdateProduct(DAL<Product> product)
+    {
+        Console.WriteLine("Digite o nome do product que voce deseja editar");
+        string productName = Console.ReadLine();
+        Product productFound = product.FindBy(a => a.Name.Equals(productName));
+        Console.WriteLine("Digite o novo nome");
+        productFound.Name = Console.ReadLine();
+        product.Update(productFound);
+    }
+    public void RemoveProduct(DAL<Product> product)
+    {
+        Console.WriteLine("Digite o nome do product que voce deseja editar");
+        string productName = Console.ReadLine();
+        Product productFound = product.FindBy(a => a.Name.Equals(productName));
+        product.Remover(productFound);
+    }
+}
