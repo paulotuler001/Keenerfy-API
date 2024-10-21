5838647b30e2fc49f3aaad25c6e1a1af690596ec
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-21 12:06:10 -0300

    feat: Updating database with IdentityUser

--- Keenerfy.Shared.Data/Migrations/20241021133340_Keenerfy.Designer.cs
+++ Keenerfy.Shared.Data/Migrations/20241021133340_Keenerfy.Designer.cs
@@ -4,6 +4,7 @@ using Keenerfy.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Keenerfy.Shared.Data.Migrations
 {
     [DbContext(typeof(KeenerfyContext))]
-    partial class KeenerfyContextModelSnapshot : ModelSnapshot
+    [Migration("20241021133340_Keenerfy")]
+    partial class Keenerfy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
