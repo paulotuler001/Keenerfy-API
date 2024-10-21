9fd39083c2b967a39304b31b204304c9b3586986
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-21 12:05:45 -0300

    feat: updating the connectionString to point to the remote API

--- Keenerfy.Shared.Data/Database/KeenerfyContext.cs
+++ Keenerfy.Shared.Data/Database/KeenerfyContext.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Keenerfy.Models;
 using Keenerfy.Shared.Models.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Keenerfy.Database;
-public class KeenerfyContext : DbContext
+public class KeenerfyContext : IdentityDbContext<User>
 {
     public DbSet<Product> Products { get; set; }
     public DbSet<Sale> Sales { get; set; }
-    public DbSet<User> Users { get; set; }
     public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
 
-    public readonly string connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=KeenerfyV2;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    //*local* public readonly string connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=KeenerfyV2;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    /* dev */ public readonly string connectionString = "Server=tcp:keenerfyserver.database.windows.net,1433;Initial Catalog=KeenerfyV2;Persist Security Info=False;User ID=guga;Password=MySqlDatabase123321@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
