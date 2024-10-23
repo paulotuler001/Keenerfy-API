44b0bba28fde2f4e34fa8f090f894f74f558c501
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-23 20:55:33 -0300

    feat: cleaning imports unused, including fields in the constructor

--- Keenerfy.Shared.Models/Models/PurchaseOrder.cs
+++ Keenerfy.Shared.Models/Models/PurchaseOrder.cs
@@ -1,27 +1,25 @@
 ﻿using Keenerfy.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Keenerfy.Shared.Models.Models;
 public class PurchaseOrder
 {
     public PurchaseOrder() { }
-    public PurchaseOrder(DateTime date, int quantity, Product product)
+    public PurchaseOrder(DateTime date, int quantity, int productId, string userId)
     {
         Date = date;
         Quantity = quantity;
-        Product = product;
+        ProductId = productId;
+        UsersId = userId;
     }
     public int Id { get; set; }
     public DateTime Date { get; set; }
 
+    public string UsersId { get; set; }
     public virtual User Users { get; set; }
 
     public int ProductId { get; set; }
     public virtual Product Product { get; set; }
+
 
     public int Quantity { get; set; }
 }
